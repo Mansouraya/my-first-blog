12f5b52bd042c9338178e14d149c77c06a857c6d
Mansouraya <mansouraya.m.pieters@gmail.com>
2015-09-19 16:16:18 +0200

    Blog aangepast

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
+from django.contrib.auth.models import User
 
 def post_list (request):
-    return render(request, 'blog/post_list.html',{})
+    posts = Post.objects.filter(author=User.objects.get(username="mansouraya"))
+    username = request.user.username
+
+    return render(request, 'blog/post_list.html', {})
